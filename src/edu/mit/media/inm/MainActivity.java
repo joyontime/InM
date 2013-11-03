daaac07d2bff95d2caf3c64c538158984df73395
Joy Chen <drgnfnix@gmail.com>
2013-11-03 18:16:24 -0500

    Initial project

---
+++ src/edu/mit/media/inm/MainActivity.java
@@ -0,0 +1,53 @@
+package edu.mit.media.inm;
+
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.app.ActionBar.TabListener;
+import edu.mit.media.inm.adapter.*;
+
+public class MainActivity extends FragmentActivity implements TabListener {
+	private ViewPager viewPager;
+    private TabsPagerAdapter mAdapter;
+    private ActionBar actionBar;
+    // Tab titles
+    private String[] tabs = { "Status", "Feed", "Update", "Tell" };
+ 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/edu/mit/media/inm/adapter/TabsPagerAdapter.java
@@ -0,0 +1,39 @@
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+ 
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+ 
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+ 
+    @Override
+    public Fragment getItem(int index) {
+ 
+        switch (index) {
+        case 0:
+            // Top Rated fragment activity
+            return new StatusFragment();
+        case 1:
+            // Games fragment activity
+            return new FeedFragment();
+        case 2:
+            // Movies fragment activity
+            return new UpdateFragment();
+        case 3:
+            // Movies fragment activity
+            return new TellFragment();
+        }
+ 
+        return null;
+    }
+ 
+    @Override
+    public int getCount() {
+        // get item count - equal to number of tabs
+        return 4;
+    }
+ 
+}
