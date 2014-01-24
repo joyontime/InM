63e13449126013be55aa1f9372e36f22b5a53fe4
Joy Chen <drgnfnix@gmail.com>
2014-01-24 16:34:09 -0500

    Capitalize basic.

--- src/edu/mit/media/inm/http/GetThread.java
+++ src/edu/mit/media/inm/http/GetThread.java
@@ -2,16 +2,10 @@ package edu.mit.media.inm.http;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringWriter;
-import java.net.Authenticator;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.PasswordAuthentication;
 import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
@@ -30,20 +24,7 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManagerFactory;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.auth.Credentials;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
-import org.apache.http.util.EntityUtils;
-
-import edu.mit.media.inm.R;
 import edu.mit.media.inm.prefs.PreferenceHandler;
 
 import android.content.Context;
@@ -154,8 +135,8 @@ public abstract class GetThread extends AsyncTask<Void, Void, String> {
 			conn.setRequestProperty("Accept-Charset", charset);
 			conn.setRequestProperty(
 					"Authorization",
-					"basic "
-							+ Base64.encode((ph.username() + ":" + ph
+					"Basic "
+							+ Base64.encodeToString((ph.username() + ":" + ph
 									.password()).getBytes(), Base64.NO_WRAP));
 
 			conn.setConnectTimeout(TIMEOUT);
