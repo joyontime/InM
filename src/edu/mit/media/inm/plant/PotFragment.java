4411ebee488c28835d18e07570e57a0399a50915
Joy Chen <drgnfnix@gmail.com>
2014-01-30 16:20:40 -0500

    Better pot color selection.

--- src/edu/mit/media/inm/plant/PotFragment.java
+++ src/edu/mit/media/inm/plant/PotFragment.java
@@ -2,42 +2,30 @@ package edu.mit.media.inm.plant;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.UUID;
-
 import edu.mit.media.inm.R;
-import edu.mit.media.inm.data.PlantDataSource;
 import edu.mit.media.inm.data.UserDataSource;
 import edu.mit.media.inm.http.PostPlant;
 import edu.mit.media.inm.prefs.PreferenceHandler;
 import edu.mit.media.inm.user.User;
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.Context;
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.SparseBooleanArray;
-import android.view.Gravity;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class PotFragment extends Fragment {
@@ -52,7 +40,7 @@ public class PotFragment extends Fragment {
 	private ListView friend_list;
 	private List<User> friends;
 	
-	private int pot_color = 1;
+	private int selected_color = 0;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -75,18 +63,42 @@ public class PotFragment extends Fragment {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		
+		int num_pots =  Plant.pots.length;
+		final Bitmap[] only_pots = new Bitmap[num_pots];
+		for (int i = 0; i <num_pots ; i++){
+			Bitmap original = BitmapFactory.decodeResource(getResources(),
+					Plant.pots[i]);
+			int START_X = 0;
+			int START_Y = original.getHeight()/2;
+			int WIDTH_PX = original.getWidth();
+			int HEIGHT_PX = original.getHeight()/2;
+
+			// Crop bitmap
+			Bitmap cropped_pot = Bitmap.createBitmap(original, START_X,
+					START_Y, WIDTH_PX, HEIGHT_PX, null, false);
+			
+			only_pots[i] = cropped_pot;
+		}
+		
 		title_box = (EditText) getView().findViewById(R.id.title_box);
 		pot_image = (ImageView) getView().findViewById(R.id.pot_image);
-		pot_image.setImageResource(Plant.pots[pot_color]);
-		pot_image.setOnClickListener(new OnClickListener(){
+		pot_image.setImageBitmap(only_pots[selected_color]);
+		
+		LinearLayout pot_list = (LinearLayout) getView().findViewById(R.id.pot_list);
+		for (int i = 0; i < num_pots; i++){
+			ImageView pot = new ImageView(ctx);
+			pot.setImageBitmap(only_pots[i]);
+			final int pot_color = i;
 
-			@Override
-			public void onClick(View arg0) {
-				if (pot_color > 11)	pot_color = 1;
-				else pot_color++;
-				pot_image.setImageResource(Plant.pots[pot_color]);
-			}
-		});
+			pot.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View arg0) {
+					pot_image.setImageBitmap(only_pots[pot_color]);
+					selected_color = pot_color;
+				}
+			});
+			pot_list.addView(pot);
+		}
 		
 		// Database call for friends
 		UserDataSource user_data = new UserDataSource(ctx);
@@ -149,7 +161,7 @@ public class PotFragment extends Fragment {
         		}
         		
         		PostPlant http_client = new PostPlant(0, ctx);
-        		http_client.setupParams(username, pot_color, share_server.toString(),
+        		http_client.setupParams(username, selected_color, share_server.toString(),
         				share_local.toString(), title_box.getText().toString());
 
                 Toast.makeText(getActivity(), "Publishing to server...", Toast.LENGTH_LONG)
