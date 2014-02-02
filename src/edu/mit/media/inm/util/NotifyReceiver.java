b8b97f797503c735ea932c664a305d424a956f7d
Joy Chen <drgnfnix@gmail.com>
2014-02-02 11:52:00 -0500

    Have first notification trigger after half a day.

--- src/edu/mit/media/inm/util/NotifyReceiver.java
+++ src/edu/mit/media/inm/util/NotifyReceiver.java
@@ -21,7 +21,8 @@ public class NotifyReceiver extends BroadcastReceiver {
 				.getSystemService(Context.ALARM_SERVICE);
 		Intent i = new Intent(context, NotifyReceiver.class);
 		PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-		am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),
+		am.setRepeating(AlarmManager.RTC_WAKEUP,
+				System.currentTimeMillis() +  AlarmManager.INTERVAL_HALF_DAY,
 				AlarmManager.INTERVAL_HALF_DAY, pi);
 	}
 
