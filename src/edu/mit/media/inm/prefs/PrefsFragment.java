98475091ced8963550416644ac4f6797db7ca457
Joy Chen <drgnfnix@gmail.com>
2014-02-05 10:56:06 -0500

    Clear menu in settings.

--- src/edu/mit/media/inm/prefs/PrefsFragment.java
+++ src/edu/mit/media/inm/prefs/PrefsFragment.java
@@ -2,6 +2,9 @@ package edu.mit.media.inm.prefs;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import edu.mit.media.inm.R;
 
 public class PrefsFragment extends PreferenceFragment {
@@ -9,9 +12,17 @@ public class PrefsFragment extends PreferenceFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
 		// Load the preferences from an XML resource
 		addPreferencesFromResource(R.xml.preferences);
 	}
+	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		menu.clear();
+	    super.onCreateOptionsMenu(menu, inflater);
+	}
+
 	
 	@Override
 	public void onResume(){
