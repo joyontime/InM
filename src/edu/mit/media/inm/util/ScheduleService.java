6aea0ea503f57b5747f3f478999063e851f1b50a
Joy Chen <drgnfnix@gmail.com>
2014-03-05 08:43:48 -0500

    Notification not working?

--- src/edu/mit/media/inm/util/ScheduleService.java
+++ src/edu/mit/media/inm/util/ScheduleService.java
@@ -54,40 +54,25 @@ public class ScheduleService extends Service {
 			alarms = new ArrayList<Calendar>();
 		}
 		
-		// Remove Alarms that already ran.
-		while (true){
-			if (alarms.size()>0){
-				if (alarms.get(0).getTimeInMillis() < System.currentTimeMillis()){
-					Log.d(TAG, "Removing: " + alarms.get(0).getTime().toString());
-					alarms.remove(0);
-				} else {
-					break;
-				}
-			} else {
-				break;
-			}
-		}
+		Calendar c = Calendar.getInstance();
+		Random random = new Random();
+		int day_hour = 9 + random.nextInt(6);
+		int night_hour = 15 + random.nextInt(6);
+		c.setTimeInMillis(System.currentTimeMillis());
 
-		if (alarms.size() < 1){
-			Calendar c = Calendar.getInstance();
-			Random random = new Random();
-			int day_hour = 9 + random.nextInt(6);
-			int night_hour = 15 + random.nextInt(6);
-			c.setTimeInMillis(System.currentTimeMillis());
-
-	    	c.set(Calendar.HOUR_OF_DAY, 15);
-	    	c.set(Calendar.MINUTE, 0);
-	    	c.set(Calendar.SECOND, 0);
-	    	
-	    	if (c.getTime().before(new Date(System.currentTimeMillis()))){
-    			c.roll(Calendar.DATE, true);
-    	    	c.set(Calendar.HOUR_OF_DAY, day_hour);
-	    	} else {
-    	    	c.set(Calendar.HOUR_OF_DAY, night_hour);
-	    	}
-			Log.d(TAG, "Next Notification: " + c.getTime().toString());
-	    	alarms.add(c);
-	    	new AlarmTask(this, c).run();
-		}
+    	c.set(Calendar.HOUR_OF_DAY, 15);
+    	c.set(Calendar.MINUTE, 0);
+    	c.set(Calendar.SECOND, 0);
+    	
+    	if (c.getTime().before(new Date(System.currentTimeMillis()))){
+			c.roll(Calendar.DATE, true);
+	    	c.set(Calendar.HOUR_OF_DAY, day_hour);
+    	} else {
+	    	c.set(Calendar.HOUR_OF_DAY, night_hour);
+    	}
+    	
+		Log.d(TAG, "Next Notification: " + c.getTime().toString());
+    	alarms.add(c);
+    	new AlarmTask(this, c).run();
 	}
 }
